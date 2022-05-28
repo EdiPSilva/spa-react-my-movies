4fb766fc8be51503a139aa4d1153e8d64885c380
DESKTOP-7PGMQOS\edipa <edipaulinodasilva@gmail.com>
2022-05-28 19:04:15 -0300

    problema ao add router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
-import { MoviesService } from "./api/MoviesService";
 import { Header } from "./components/Header";
-import { MoviesList } from "./components/MoviesList";
-import { MoviesContainer } from "./styles/MoviesContainer";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from "./views/Home";
+import { About } from "./views/About";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const fetchMovies = async () => {
-    const {data} = await MoviesService.getMovies();
-    setMovies(data.results);
-  };
-
-  useEffect(() => {
-    fetchMovies();
-  }, []);
-
+  
   return (
     <div className="App">
-      <Header />   
-
-      <MoviesContainer>
-        <MoviesList movies={movies} />
-      </MoviesContainer>   
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/about" component={ <About />} />
+          <Route exact path="/" component={ <Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
