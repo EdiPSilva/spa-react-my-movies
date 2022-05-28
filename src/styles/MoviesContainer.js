422ae5a551167024d607f9423dbdbec960d24c8f
DESKTOP-7PGMQOS\edipa <edipaulinodasilva@gmail.com>
2022-05-28 17:35:58 -0300

    criando os componentes header, main menu, movies list e movie item e tambem os seus styles

---
+++ src/styles/MoviesContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MoviesContainer = styled.section`
+    padding: 15px;
+    margin: 15px;
+    backgroud: #eee;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+`;
